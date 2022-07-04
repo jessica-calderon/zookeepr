eada7b81c3498a9ff37492bfda339bab40d1486d
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-04 00:38:29 -0500

    test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const { animals } = require('./data/animals');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
